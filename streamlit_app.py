a54271ce63dc7bdb87c161cd49529779d83efd40
rwgarcia85 <162874199+rwgarcia85@users.noreply.github.com>
2024-12-09 21:22:32 +0800

    Update streamlit_app.py
    added requests package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -23,6 +24,7 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
+
 if ingredients_list:
     #st.write(ingredients_list)
     #st.text(ingredients_list)
@@ -43,3 +45,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
+        
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)

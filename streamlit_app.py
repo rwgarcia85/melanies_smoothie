fdeadedc7f496c16d0d606e914ffda77ce7522d1
rwgarcia85 <162874199+rwgarcia85@users.noreply.github.com>
2024-12-09 21:51:24 +0800

    Update streamlit_app.py
    bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,15 +27,12 @@ ingredients_list = st.multiselect(
 
 
 if ingredients_list:
-    #st.write(ingredients_list)
-    #st.text(ingredients_list)
-
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on=pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')

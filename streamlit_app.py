fe8d4331c479324a698160e70aaba8734758f7ed
rwgarcia85 <162874199+rwgarcia85@users.noreply.github.com>
2024-12-09 21:41:59 +0800

    Update streamlit_app.py
    test search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write('The on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 #session = get_active_session()
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('search_on'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
     , my_dataframe
